b4f89a6a3a8092533a6783cf33fe3b3b042f8936
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-05 14:41:04 -0300

    create ILens interface

--- src/models/Connection.ts
+++ src/models/Connection.ts
@@ -5,3 +5,5 @@ const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI
   || 'mongodb://root:example@localhost:27017/glassesStore?authSource=admin'
 ) => mongoose.connect(mongoDatabaseURI)
+
+export default connectToDatabase;
