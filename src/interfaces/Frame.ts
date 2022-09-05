9cbf56750afe4566a4d3e71fdcdd048516bb681d
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-05 14:44:14 -0300

    create IFrame interface

---
+++ src/interfaces/Frame.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const frameZodSchema = z.object({
+  material: z.string(),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string'
+  }).min(3, { message: 'Color must be 3 or more characters long'})
+})
+
+type IFrame = z.infer<typeof frameZodSchema>
+
+export default IFrame;
